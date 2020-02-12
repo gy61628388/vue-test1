90889f30b154868be8056f71313922b23e178d91
gy61628388 <310883101@qq.com>
2020-02-13 01:05:36 +0800

    husky

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,21 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+// import { shallowMount } from "@vue/test-utils";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
+// describe("HelloWorld.vue", () => {
+//   it("renders props.msg when passed", () => {
+//     const msg = "new message";
+//     const wrapper = shallowMount(HelloWorld, {
+//       propsData: { msg }
+//     });
+//     expect(wrapper.text()).toMatch(msg);
+//   });
+// });
+function add(num1,num2){
+  return num1 + num2
+}
+
+describe('加法测试',()=>{
+  it('数字相加',()=>{
+    expect(add(1,2)).toBe(4)
+  })
+})
